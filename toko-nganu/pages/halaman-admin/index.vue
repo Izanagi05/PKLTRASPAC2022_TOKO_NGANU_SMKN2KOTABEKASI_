961d7a15b4d3993ff0b2167420efb69fdd77a169
AdhifCode <nadhifalazharuddiya@gmail.com>
2023-02-27 13:08:30 +0700

    committ

--- toko-nganu/pages/halaman-admin/index.vue
+++ toko-nganu/pages/halaman-admin/index.vue
@@ -1,59 +1,51 @@
 <template>
   <div>
+    <NavbarPolos />
     <v-dialog v-model="dialogDeleteuser" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5">Yakin ingin menghapus user?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDeleteuser">Cancel</v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="confirmhapususer">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-</v-dialog>
+      <v-card>
+        <v-card-title class="text-h5">Yakin ingin menghapus user?</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue-darken-1" variant="text" @click="closeDeleteuser"
+            >Cancel</v-btn
+          >
+          <v-btn color="blue-darken-1" variant="text" @click="confirmhapususer"
+            >OK</v-btn
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-dialog v-model="dialogDeletetoko" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5">Yakin ingin menghapus Toko ini?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDeletetoko">Cancel</v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="confirmhapustoko">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-</v-dialog>
-    <div class="bar">
-      <div class="contact white--text">
-        <div class="contact-text font-weight-regular">+62-000-000-000</div>
-      </div>
-    </div>
+      <v-card>
+        <v-card-title class="text-h5"
+          >Yakin ingin menghapus Toko ini?</v-card-title
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue-darken-1" variant="text" @click="closeDeletetoko"
+            >Cancel</v-btn
+          >
+          <v-btn color="blue-darken-1" variant="text" @click="confirmhapustoko"
+            >OK</v-btn
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <div class="home-container">
-      <v-row>
-        <v-col cols="1">
-          <div class="">
-            <v-img :src="require('~/assets/logo.png')" width="100px"></v-img>
-          </div>
-        </v-col>
-        <v-col>
-          <div class="store-name f24sb">
-            <div>Toko Nganu</div>
-            <div v-for="(asr, index) in alluser" :key="index">
-            {{ asr.nama }}
-            </div>
-          </div>
-        </v-col>
-        <v-col>
-          <div v-for="(atr, index) in alltoko" :key="index">
-          {{ atr.nama }}  ====== <v-btn @click="hapustoko(atr)">hapus</v-btn>
-          </div>
-          <!-- <v-text-field
+      <div v-for="(asr, index) in alluser" :key="index">
+        {{ asr.nama }}
+      </div>
+      <div v-for="(atr, index) in alltoko" :key="index">
+        {{ atr.nama }} ====== <v-btn @click="hapustoko(atr)">hapus</v-btn>
+      </div>
+      <!-- <v-text-field
             class="search"
             rounded
             placeholder="Search nganu"
             append-icon="mdi-magnify"
           ></v-text-field>           -->
-        </v-col>
-      </v-row>
       <div class="content">
         <div class="kiri">
           <div class="sub-title ml-5" width="170px" height="30px">
@@ -266,92 +258,95 @@
 <script>
 import axios from "axios";
 export default {
-  middleware: ['middlewareku', 'middlewarerole'],
-data() {
-  return {
-      alluser:{},
-      alltoko:{},
+  middleware: ["middlewareku", "middlewarerole"],
+  data() {
+    return {
+      alluser: {},
+      alltoko: {},
 
-      dialogDeleteuser:false,
-      dialogDeletetoko:false,
-      editedIndex:0,
-      detaildatadialoguser:{
-        id:''
+      dialogDeleteuser: false,
+      dialogDeletetoko: false,
+      editedIndex: 0,
+      detaildatadialoguser: {
+        id: "",
       },
-      detaildatadialogtoko:{
-        toko_id:''
-      }
-  }
-},
+      detaildatadialogtoko: {
+        toko_id: "",
+      },
+    };
+  },
 
   methods: {
-    getalluser(){
-      axios.get('http://127.0.0.1:8000/api/getalluser').then(respon=>{
-        console.log(respon)
-        this.alluser = respon.data
-      })
+    getalluser() {
+      axios.get("http://127.0.0.1:8000/api/getalluser").then((respon) => {
+        console.log(respon);
+        this.alluser = respon.data;
+      });
     },
-    hapususer (item) {
-        this.editedIndex = this.alluser.indexOf(item)
-        this.detaildatadialoguser = Object.assign({}, item)
-        this.dialogDeleteuser = true
-        console.log(item)
-      },
-    confirmhapususer(){
-      axios.delete('http://127.0.0.1:8000/api/deleteuser/'+this.detaildatadialoguser.id).then(respon =>{
-       console.log(respon)
-        alert('berhasil hapus')
-        })
-        this.alluser.splice(this.editedIndex, 1)
-        this.closeDeleteuser()
-
-  },
-  closeDeleteuser () {
-        this.dialogDeleteuser = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-
-
-      getalltoko(){
-      axios.get('http://127.0.0.1:8000/api/getalltoko').then(respon=>{
-        console.log(respon)
-        this.alltoko =respon.data
-      })
+    hapususer(item) {
+      this.editedIndex = this.alluser.indexOf(item);
+      this.detaildatadialoguser = Object.assign({}, item);
+      this.dialogDeleteuser = true;
+      console.log(item);
     },
-    hapustoko (item) {
-        this.editedIndex = this.alluser.indexOf(item)
-        this.detaildatadialogtoko = Object.assign({}, item)
-        this.dialogDeletetoko = true
-        console.log(item)
-      },
-    confirmhapustoko(){
-      axios.delete('http://127.0.0.1:8000/api/hapustoko/'+this.detaildatadialogtoko.toko_id).then(respon =>{
-       console.log(respon)
-        alert('berhasil hapus')
-        })
-        this.alltoko.splice(this.editedIndex, 1)
-        this.closeDeletetoko()
+    confirmhapususer() {
+      axios
+        .delete(
+          "http://127.0.0.1:8000/api/deleteuser/" + this.detaildatadialoguser.id
+        )
+        .then((respon) => {
+          console.log(respon);
+          alert("berhasil hapus");
+        });
+      this.alluser.splice(this.editedIndex, 1);
+      this.closeDeleteuser();
+    },
+    closeDeleteuser() {
+      this.dialogDeleteuser = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
 
-  },
-  closeDeletetoko () {
-        this.dialogDeletetoko = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-
+    getalltoko() {
+      axios.get("http://127.0.0.1:8000/api/getalltoko").then((respon) => {
+        console.log(respon);
+        this.alltoko = respon.data;
+      });
+    },
+    hapustoko(item) {
+      this.editedIndex = this.alluser.indexOf(item);
+      this.detaildatadialogtoko = Object.assign({}, item);
+      this.dialogDeletetoko = true;
+      console.log(item);
+    },
+    confirmhapustoko() {
+      axios
+        .delete(
+          "http://127.0.0.1:8000/api/hapustoko/" +
+            this.detaildatadialogtoko.toko_id
+        )
+        .then((respon) => {
+          console.log(respon);
+          alert("berhasil hapus");
+        });
+      this.alltoko.splice(this.editedIndex, 1);
+      this.closeDeletetoko();
+    },
+    closeDeletetoko() {
+      this.dialogDeletetoko = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
   },
   created() {
-    this.getalluser()
-    this.getalltoko()
+    this.getalluser();
+    this.getalltoko();
   },
-}
+};
 </script>
 
 <style>
--- toko-nganu/pages/user-view/tambah-toko/index.vue
+++ toko-nganu/pages/user-view/tambah-toko/index.vue
@@ -1,44 +1,66 @@
 <template>
   <div>
-
-    <h1>Tambah toko</h1>
-    <v-text-field v-model="databarang.nama" placeholder="Nama"></v-text-field>
-    <v-text-field v-model="databarang.alamat" placeholder="Alamat"></v-text-field>
-    <v-text-field v-model="databarang.deskripsi" placeholder="Deskripsi"></v-text-field>
-    <v-text-field v-model="databarang.no_telepon" placeholder="No Telepon"></v-text-field>
-    <v-text-field v-model="databarang.logo" placeholder="logo"></v-text-field>
-    <v-btn @click="buattoko()">buat</v-btn>
+    <NavbarPolos />
+    <div class="tambah-container">
+      <h1>Tambah toko</h1>
+      <v-text-field v-model="databarang.nama" placeholder="Nama"></v-text-field>
+      <v-text-field
+        v-model="databarang.alamat"
+        placeholder="Alamat"
+      ></v-text-field>
+      <v-text-field
+        v-model="databarang.deskripsi"
+        placeholder="Deskripsi"
+      ></v-text-field>
+      <v-text-field
+        v-model="databarang.no_telepon"
+        placeholder="No Telepon"
+      ></v-text-field>
+      <v-text-field v-model="databarang.logo" placeholder="logo"></v-text-field>
+      <v-btn @click="buattoko()">buat</v-btn>
+    </div>
   </div>
 </template>
+
+<style>
+.tambah-container {
+  padding: 0px 123px;
+}
+</style>
+
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  middleware:'middlewareku',
+  middleware: "middlewareku",
   data() {
     return {
-
-      databarang:{
-        userid:null,
-        nama:null,
-        alamat:null,
-        deskripsi:null,
-        no_telepon:null,
-        logo:null,
-      }
-    }
+      databarang: {
+        userid: null,
+        nama: null,
+        alamat: null,
+        deskripsi: null,
+        no_telepon: null,
+        logo: null,
+      },
+    };
   },
 
   methods: {
-    buattoko(){
-      axios.post('http://127.0.0.1:8000/api/createtoko/'+ this.userid, this.databarang).then(respon=> {
-        this.$router.push('/user-view/toko-user')
-        console.log(respon)
-      })
-    }
-  },
-  created(){
-     const  usid = this.$cookies.get('cookieku')
-    this.userid = usid.data.id
+    buattoko() {
+      axios
+        .post(
+          "http://127.0.0.1:8000/api/createtoko/" + this.userid,
+          this.databarang
+        )
+        .then((respon) => {
+          this.$router.push("/user-view/toko-user");
+          console.log(respon);
+        });
     },
-}
+  },
+  created() {
+    const usid = this.$cookies.get("cookieku");
+    this.userid = usid.data.id;
+  },
+};
 </script>
